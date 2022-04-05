df5007bdc509d72f6080056819cca588c0f422b8
DmytroBalanetskiy <dbalanetskyi@flexservice.com>
2022-04-05 17:07:49 +0300

    add some fix

--- Tasks/HtmlHelper/TasksHelper.cs
+++ Tasks/HtmlHelper/TasksHelper.cs
@@ -5,7 +5,7 @@ namespace Tasks.HtmlHelper
 {
     public static class TasksHelper
     {
-        public static HtmlString StateName(this IHtmlHelper html, string stateName, string className) => 
-            new HtmlString($@"<div><p class=""{className}"">{stateName}</p></div>");
+        public static HtmlString StateName(this IHtmlHelper html, string stateName) => 
+            new HtmlString($@"<div><p class=""mt-3 mb-2 fw-bold border-bottom"">{stateName}</p></div>");
     }
 }
